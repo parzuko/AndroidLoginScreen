98c7abcd95a72bd531b87e908dcb3895dc6edd40
parzuko <jivansh17@gmail.com>
2020-05-06 20:02:13 +0530

    Landing Page

---
+++ app/src/main/java/com/example/mainacivity/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mainacivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/mainacivity/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mainacivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            val email = email_editText_register.text.toString()
+            val password = password_editText_register.text.toString()
+
+            Log.d("MainActivity","Email is: $email")
+            Log.d("MainActivity","Password is : $password")
+        }
+
+        already_have_account_textView.setOnClickListener {
+            Log.d("MainActivity","Try to show login activity")
+        }
+    }
+}
