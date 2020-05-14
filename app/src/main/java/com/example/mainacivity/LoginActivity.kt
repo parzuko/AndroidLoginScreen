43da426ac53dd507ab3bbc4c09fde1ae5854d2e8
parzuko <jivansh17@gmail.com>
2020-05-14 12:38:30 +0530

    Add Functionality to Login Page

--- app/src/main/java/com/example/mainacivity/LoginActivity.kt
+++ app/src/main/java/com/example/mainacivity/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mainacivity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,16 @@ class LoginActivity : AppCompatActivity(){
 
         setContentView(R.layout.activity_login)
 
+        login_button_login.setOnClickListener {
+            val email = email_textView_login.text.toString()
+            val password = password_textview_login.text.toString()
+
+            Log.d("Login","Attempt to login w email/pw $email/***")
+        }
+
+        backtoregister_textview_login.setOnClickListener {
+            finish()
+        }
 
     }
 }
